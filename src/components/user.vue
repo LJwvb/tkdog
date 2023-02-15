7ebb5ba14864b658efd372aab3de9a0ab7208729
eaogon <eaogoyang@163.com>
2023-02-15 20:39:15 +0800

    test

--- src/components/user.vue
+++ src/components/user.vue
@@ -14,6 +14,7 @@
                   <el-icon>
                     <User />
                   </el-icon>
+                  
                   <span >个人资料</span>
                 </el-menu-item>
                 <el-menu-item index="2">
