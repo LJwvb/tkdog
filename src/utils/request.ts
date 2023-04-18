e2c97c1f0f6475324cfe935afbc2a38e6cee093f
LJwvb <z751368560@126.com>
2023-04-18 22:47:52 +0800

    fix:build

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const baseUrl = 'http://127.0.0.1:7001/api';
-const onlineUrl = 'http://47.97.98.228:7001/api/';
+const onlineUrl = 'http://47.97.98.228:7001/api';
 
 export const request = (method: string, url: string, data?: any) => {
   return new Promise((resolve, reject) => {
