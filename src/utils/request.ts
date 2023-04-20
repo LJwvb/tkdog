5c5222412104000c62a315b947491b03b68ad290
LJwvb <z751368560@126.com>
2023-04-20 23:13:51 +0800

    fix

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
-const baseUrl = 'http://127.0.0.1:7001/api';
-// const onlineUrl = 'http://47.97.98.228:7001/api';
+// import axios from 'axios';
+// const baseUrl = 'http://127.0.0.1:7001/api';
+const onlineUrl = 'http://47.97.98.228:7001/api';
 
 export const request = (method: string, url: string, data?: any) => {
   return new Promise((resolve, reject) => {
     axios({
       method,
-      url: baseUrl + url,
+      url: onlineUrl + url,
       ...data,
     })
       .then((res) => {
